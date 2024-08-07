985e071250a5300523ed5e0195b316bfedb3bcad
ItsThanhTung <tung.10hoa.2016@gmail.com>
2024-08-07 15:54:51 +0700

    Remove unused file

--- preprocess_data/get_json_len.py
+++
@@ -1,9 +0,0 @@
-import json
-
-json_path = "/lustre/scratch/client/vinai/users/tungdt33/ARP/code/segment-arp/val_private_2024-07-01.json"
-
-
-with open(json_path) as json_data:
-    real_data = json.load(json_data)
-
-print(f"LENGTH={len(real_data)}")
